004ed5435fa5200524d0b6ee7d3392e7701e493d
Alberto Colombara <albertocolombara@hotmail.com>
2024-01-24 23:37:38 -0300

    efeito de scroll no header e mudanças nos headings

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,12 @@ tituloFaq.forEach(i => {
     })
 })
 
+const header = document.querySelector("header");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+};
