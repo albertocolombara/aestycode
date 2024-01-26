5b1aa73fb611a5ef3734517274ca2fb1a0a2fce8
Alberto Colombara <albertocolombara@hotmail.com>
2024-01-25 23:49:08 -0300

    efeito noise, luz e background

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,10 @@ tituloFaq.forEach(i => {
 
 const header = document.querySelector("header");
 
-window.onscroll = function() {
+window.addEventListener("scroll", () => {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
     }
-};
+});
